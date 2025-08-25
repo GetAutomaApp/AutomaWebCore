36322c21068d438b13be4f2176dc0d74dfbc7120
William <vimscientist69@gmail.com>
2025-08-25 13:28:09 -0600

    feat(SeleniumGridNodeAutoscaler): automatically stop machine after 1 session, refactored code a bit

---
+++ SeleniumGridNodeMachineAutoscaler/Sources/SeleniumGridNodeMachineAutoscaler/SeleniumGridNodeAppInteractor.swift
@@ -0,0 +1,33 @@
+// SeleniumGridNodeAppInteractor.swift
+// Copyright (c) 2025 GetAutomaApp
+// All source code and related assets are the property of GetAutomaApp.
+// All rights reserved.
+
+import Vapor
+
+internal protocol SeleniumGridNodeAppInteractorBase {
+    var nodesAppMachineAPIURL: String { get }
+    var flyAPIToken: String { get }
+}
+
+internal class SeleniumGridNodeAppInteractor: SeleniumGridNodeAppInteractorBase {
+    let nodesAppMachineAPIURL: String
+    let flyAPIToken: String
+
+    init() throws {
+        guard
+            let urlString = Environment.get("FLY_API_URL"),
+            let flyAPIURL = URL(string: urlString)
+        else {
+            throw Abort(.internalServerError)
+        }
+        nodesAppMachineAPIURL = "\(flyAPIURL.absoluteString)/v1/apps/automa-web-core-seleniumgrid-node/machines"
+
+        guard
+            let flyAPIToken = Environment.get("SELENIUM_GRID_NODE_FLY_APP_API_TOKEN")
+        else {
+            throw Abort(.internalServerError)
+        }
+        self.flyAPIToken = flyAPIToken
+    }
+}
