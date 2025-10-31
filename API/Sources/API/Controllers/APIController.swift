2c1736ed16ddc9f7348b9fb82c36ec61cd34821e
William <vimscientist69@gmail.com>
2025-10-31 09:30:25 +0200

    fix: move webcore api payload to automa utilities as it is a re-usable data type

--- API/Sources/API/Controllers/APIController.swift
+++ API/Sources/API/Controllers/APIController.swift
@@ -3,6 +3,7 @@
 // All source code and related assets are the property of GetAutomaApp.
 // All rights reserved.
 
+import AutomaUtilities
 import Vapor
 
 internal struct APIController: RouteCollection {
@@ -17,7 +18,7 @@ internal struct APIController: RouteCollection {
 
     @Sendable
     public func get(req: Request) async throws -> String {
-        let payload = try req.content.decode(APIEndpointPayload.self)
+        let payload = try req.content.decode(AutomaWebCoreAPIEndpointPayload.self)
         return try await WebBrowserClient(logger: req.logger, payload: payload).getHTML()
     }
 
@@ -41,44 +42,5 @@ internal struct APIController: RouteCollection {
     @Sendable
     public func delete(req _: Request) async throws -> String {
         "hello world"
-    }
-}
-
-public struct APIEndpointPayload: Content {
-    let url: URL
-    let scrollToBottom: Bool
-
-    public init(from decoder: any Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        url = try values.decode(URL.self, forKey: .url)
-        guard
-            let stb = try? values.decode(Bool.self, forKey: .url)
-        else {
-            scrollToBottom = false
-            return
-        }
-        scrollToBottom = stb
-    }
-
-    public init(url: URL, scrollToBottom: Bool = false) {
-        self.url = url
-        self.scrollToBottom = scrollToBottom
-    }
-
-    // configuration options required to be implemented and handled in route handlers when
-    // a service that needs to be able to make a request both with a browser (jsRender) and without a browser
-    // (jsRender=false),
-    //
-
-    // let jsRender: Bool
-    // let residentialProxy: Bool
-    // let autoCaptchaSolving: Bool
-
-    public enum CodingKeys: String, CodingKey {
-        case url
-        case scrollToBottom = "scroll_to_bottom"
-        // case jsRender = "js_render"
-        // case residentialProxy = "residential_proxy"
-        // case autoCaptchaSolving = "auto_captcha_solving"
     }
 }
